85f5ed98244980ff9976f6ecee04fb2fd7999e63
Elementx Development <128922966+ElementxDev@users.noreply.github.com>
2023-04-14 03:41:04 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL_LOCAL = import.meta.env.VITE_API_LOCAL;
-export const API_URL_ONLINE = "mongodb+srv://elementx-admin:admin1608@cluster0.ybgln.mongodb.net/test";
+export const API_URL_ONLINE = "mongodb+srv://elementx-admin:admin1608@cluster0.ybgln.mongodb.net/elementxbd";
 export const ONLINE_HOST = import.meta.env.VITE_HOST;
